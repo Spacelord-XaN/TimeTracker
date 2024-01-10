7cdb9cf78c64e1be052e584cf16d67522a31a233
Alexander Kastler <github@xaka.eu>
2024-01-10 17:33:26 +0000

    Introduce resume command #15

---
+++ src/TimeTracker/Commands/ResumeCommand.cs
@@ -0,0 +1,56 @@
+﻿using System.CommandLine;
+using Microsoft.EntityFrameworkCore;
+using Xan.TimeTracker.Data;
+using Xan.TimeTracker.Models;
+
+namespace Xan.TimeTracker.Commands;
+
+public static class ResumeCommand
+{
+    public static Command Build()
+    {
+        Option<DateOnly?> dateOption = new("--date");
+        dateOption.AddAlias("-d");
+        Option<TimeOnly?> timeOption = new("--time");
+        timeOption.AddAlias("-t");
+
+        Command cmd = new("resume")
+        {
+            dateOption,
+            timeOption
+        };
+        cmd.SetHandler(ResumeAsync, dateOption, timeOption);
+
+        return cmd;
+    }
+
+    private static async Task ResumeAsync(DateOnly? date, TimeOnly? time)
+    {
+        TimeTrackerDb db = await Helpers.GetDbAsync();
+        if (await db.IsRunningAsync())
+        {
+            ConsoleUi.Error("Cannot resume while running.");
+            return;
+        }
+
+        TimeEntry? lastEntry = await db.Entries.OrderByDescending(entry => entry.End).FirstOrDefaultAsync();
+        if (lastEntry == null)
+        {
+            ConsoleUi.Error("Not entry found for resuming.");
+            return;
+        }
+
+        DateTime timestamp = Helpers.GetTimestamp(date, time);
+        TimeEntry entry = new()
+        {
+            Comment = lastEntry.Comment,
+            ProjectName = lastEntry.ProjectName,
+            Start = timestamp
+        };
+
+        db.Entries.Add(entry);
+        await db.SaveChangesAsync();
+
+        ConsoleUi.StartedEntry(entry);
+    }
+}
--- src/TimeTracker/Program.cs
+++ src/TimeTracker/Program.cs
@@ -15,6 +15,7 @@ public static class Program
             StartCommand.Build(),
             StatusCommand.Build(),
             StopCommand.Build(),
+            ResumeCommand.Build()
         };
         await rootCommand.InvokeAsync(args);
 
